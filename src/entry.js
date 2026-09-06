cd8eb9c1b0e0734afc17bab5e898aa98ddbc234a
phanthuanxtra-v9 <phanthuanmodelactor@gmail.com>
2026-09-06 10:14:34 +0700

    feat: route Telegram vehicle publishing API

--- src/entry.js
+++ src/entry.js
@@ -1,8 +1,11 @@
 import legacy from "./index.js";
 import { handleCmsApi } from "./cms.js";
+import { handleTelegramApi } from "./telegram.js";
 
 export default {
   async fetch(request, env, ctx) {
+    const telegramResponse = await handleTelegramApi(request, env);
+    if (telegramResponse) return telegramResponse;
     const cmsResponse = await handleCmsApi(request, env);
     if (cmsResponse) return cmsResponse;
     return legacy.fetch(request, env, ctx);
